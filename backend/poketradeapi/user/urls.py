7f86e61f081be7b862869c9da8b54485d3e5105b
Minjae Lee <mjlee041@gmail.com>
2025-04-07 19:37:15 -0400

    added trade functionality and trade request functionality

--- backend/poketradeapi/user/urls.py
+++ backend/poketradeapi/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns =[
             path('<int:user_id>/',views.get_user,name = 'get_user'),
-            path('create/', views.create_user, 'create_user'),
+            path('create/', views.create_user, name = 'create_user'),
 
                ]
