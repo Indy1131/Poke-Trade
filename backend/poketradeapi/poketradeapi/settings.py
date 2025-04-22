457bbf6b615b566a5d5fbbf4115a19906fac7bff
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-04-22 00:59:11 -0400

    web deployment onfigurations

--- backend/poketradeapi/poketradeapi/settings.py
+++ backend/poketradeapi/poketradeapi/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -114,12 +117,23 @@ WSGI_APPLICATION = 'poketradeapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+USE_SQLITE = os.getenv("USE_SQLITE", "True") == "True"
+
+if USE_SQLITE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='postgresql://postgres:postgres@localhost:5432/poketradeapi',
+            conn_max_age=600
+        )
+    }
 
 
 # Password validation
