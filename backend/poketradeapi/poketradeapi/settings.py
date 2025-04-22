73a80f42eb5821e7b622e50f25ca3bc6fc58652f
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-04-22 05:36:50 -0400

    update cors access

--- backend/poketradeapi/poketradeapi/settings.py
+++ backend/poketradeapi/poketradeapi/settings.py
@@ -77,6 +77,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
      'http://127.0.0.1:5173',
+     'https://poketradegt.onrender.com'
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
