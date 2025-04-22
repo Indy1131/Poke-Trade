ab77697832561fe90a6b5128a0b9e2055fa1ee75
Tyler Tabarovsky <tyler.tabarovsky7@gmail.com>
2025-04-22 04:33:17 -0400

    setting configure for web deployment

--- backend/poketradeapi/poketradeapi/settings.py
+++ backend/poketradeapi/poketradeapi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-yqpw==8p)r7_s!3k^_j%#=qq&_d=j%%i4k1$+p6#ygo&gt#auv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['poke-trade-backend.onrender.com']
+ALLOWED_HOSTS = ['poke-trade-backend.onrender.com','127.0.0.1','localhost']
 
 
 # Application definition
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -166,7 +167,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
