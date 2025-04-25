63227396c4492a32cf0413280f7ce78d9f2194d0
Indy1131 <62616129+Indy1131@users.noreply.github.com>
2025-04-25 15:29:32 -0400

    added swarch fields to admin routes

--- backend/poketradeapi/trade/admin.py
+++ backend/poketradeapi/trade/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Trade, Transaction, Listing, Pokemon
 
 admin.site.register(Trade)
-admin.site.register(Transaction)
 admin.site.register(Listing)
-admin.site.register(Pokemon)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    search_fields =['buyer__username','seller__username']
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    search_fields =['owner_user__username']
 
 # Register your models here.
